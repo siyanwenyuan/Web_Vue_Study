81ce4422f99a52789a8c0372295381a8d5b7f974
chenwan <3335018910@qq.com>
2023-10-25 16:58:22 +0800

    Vue组件化开发方式

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>父组件中的数据{{childDate}}</p>
+    <HelloWorld
+        msg="Welcome to Your Vue.js App"
+        :count="parentCount"
+        @child-count-change="handler"
+    >
+      这是默认内容1
+    </HelloWorld>
+    <HelloWorld>这是默认内容2</HelloWorld>
+    <HelloWorld>这是默认内容3</HelloWorld>
+    <HelloWorld></HelloWorld>
+
   </div>
 </template>
 
@@ -11,7 +22,18 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  data() {
+    return {
+      parentCount: 1000,
+      childDate:0
+    }
+  },
+  methods:{
+    handler(childCount){
+      this.childDate=childCount
+    }
   }
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,40 @@
 <template>
   <div class="hello">
+
+
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <slot>基础的默认内容</slot>
+    <slot name="footer">footer的默认内容</slot>
+    <p>props中的count{{ count }}</p>
+    <button @click="handler">按钮</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  //组件通信
+  //父传子props
+  //子传父： 通过自定义事件处理
   props: {
-    msg: String
+    msg: String,
+    count: {
+      type: [String, Number],
+      // default: 100
+      required: true
+    }
+  },
+  data(){
+    return{
+      childCount: 0
+    }
+  },
+  methods:{
+    handler(){
+      this.childCount++
+      //触发自定义事件
+      this.$emit("child-count-change",this.childCount)
+    }
   }
 }
 </script>
@@ -44,14 +44,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
